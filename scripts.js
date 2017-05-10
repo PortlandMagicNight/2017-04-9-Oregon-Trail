19d9ad53d4c03e1bd15fbd511fbd06338afa33a8
dgriego <daniel@jackrabbitsystems.com>
2017-05-09 21:01:38 -0700

    adding game object

--- scripts.js
+++ scripts.js
@@ -1,1 +1,30 @@
 window.prompt("Welcome to Oregon Trail!\nWhat do you want to do?")
+
+var Game = {
+  setup: function() {
+    var eventLoop = [];
+
+    for(var i = 0; i <= 27; i++) {
+      var event = 'event ' + i;
+      eventLoop.push(event);
+
+      for(var j = 0; j < 3; j++) {
+        var td = 'td ' + j;
+        eventLoop.push(td);
+      }
+    }
+
+    return eventLoop;
+  },
+  run: function(eventLoop, party) {
+    for(var i = 0; i < eventLoop.length; i++) {
+      eventLoop[i].call(party);
+      if(party.health <= 0) {
+        alert('Game Over!');
+        return false;
+      }
+    }
+
+    alert('You made it! Welcome to Oregon City');
+  }
+}
